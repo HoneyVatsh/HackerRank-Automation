7cf155e22484463790613fee799d23a8794267de
HoneyVatsh <94510903+HoneyVatsh@users.noreply.github.com>
2021-11-17 11:02:57 +0530

    Add files via upload

---
+++ codes.js
@@ -0,0 +1,20 @@
+const { mainModule } = require("process");
+
+module.exports = {
+    answers : 
+        `#include <iostream>
+        using namespace std;
+
+        int main() {
+        
+           long long int n,sum = 0;
+            cin >> n;
+            for(int i = 0; i < n; i++){
+                long long int a;cin >> a;
+                sum+=a;
+            }
+            cout << sum << endl;
+            return 0;
+        }
+    `
+}
